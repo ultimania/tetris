ca0af81ec21e86e94c8575a348cb2067817eed2a
Fukaya Takumi <fukaya-takumi@sysj.co.jp>
2022-12-12 10:08:32 +0900

    Add pause button

--- tetris.js
+++ tetris.js
@@ -3,13 +3,29 @@
 import { Game } from "./Game.js";
 import { Asset } from "./Asset.js";
 
-const SATRT_BTN_ID = "start-btn";
+const START_BTN_ID = "start-btn";
+const PAUSE_BTN_ID = "pause-btn";
 
 window.onload = function () {
-    Asset.init()
-    let game = new Game()
-    document.getElementById(SATRT_BTN_ID).onclick = function () {
+    Asset.init();
+    let game = new Game();
+    const startButton = document.getElementById(START_BTN_ID);
+    const pauseButton = document.getElementById(PAUSE_BTN_ID);
+
+    startButton.onclick = function () {
         game.start()
+        pauseButton.textContent = "PAUSE";
         this.blur() // focus of button out
-    }
+    };
+    pauseButton.onclick = function () {
+        if (!game.paused) {
+            game.pause();
+            this.textContent = "RESTART";
+            this.blur() // focus of button out
+        } else {
+            game.restart();
+            this.textContent = "PAUSE";
+            this.blur() // focus of button out
+        }
+    };
 }
